bd6aff9ba6be1bd144b30e7e5e9650330d0476da
Kade Angell <80499246+ArKade523@users.noreply.github.com>
2023-02-11 17:02:46 -0700

    Delete build directory

--- build/CMakeFiles/BasicGame.dir/sources/Movement.cpp.o.d
+++
@@ -1,5 +0,0 @@
-CMakeFiles/BasicGame.dir/sources/Movement.cpp.o: \
-  /Users/kadeangell/Documents/Raylib/sources/Movement.cpp \
-  /usr/local/include/raylib.h \
-  /Library/Developer/CommandLineTools/usr/lib/clang/14.0.0/include/stdarg.h \
-  /Users/kadeangell/Documents/Raylib/sources/../headers/globals.h
